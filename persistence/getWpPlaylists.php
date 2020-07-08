452b15a08f340e33cc95ebba7f5e1ce5a34a0d89
Simeon Borko <simeon.borko@gmail.com>
2020-07-08 15:32:38 +0200

    WP playlists

--- persistence/getWpPlaylists.php
+++ persistence/getWpPlaylists.php
@@ -7,5 +7,35 @@ use SimeonBorko\WpYoutubeAgent\Entity;
 // param mysqli: mysqli object
 // return: list of WpPlaylist
 function getWpPlaylists($mysqli) {
-  // TODO
+  $query = <<<'SQL'
+    SELECT
+      TAX.term_id AS id,
+      `name` AS title,
+      `description`,
+      OPT.option_value AS image_url
+    FROM wp_term_taxonomy TAX
+    INNER JOIN wp_terms TER
+      ON TAX.taxonomy = "sermons-category" AND TAX.term_id = TER.term_id
+    LEFT JOIN wp_options OPT
+      ON OPT.option_name = CONCAT("sermons-category", TER.term_id, "_image_term_id")
+SQL;
+  
+  $result = $mysqli->query($query);
+  if (!$result) {
+    throw new \Exception("WP playlists could not be queried");
+  }
+  
+  $playlists = [];
+  while ($row = $result->fetch_assoc()) {
+      $p = new Entity\WpPlaylist();
+      $p->id = $row["id"];
+      $p->title = $row["title"];
+      $p->description = $row["description"];
+      $p->imageUrl = $row["image_url"];
+      $playlists[] = $p;
+  }
+  $result->close();
+  
+  return $playlists;
+  
 }
---
+++ tests/getWpPlaylists.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once "../entity/WpPlaylist.php";
+require_once "../persistence/getWpPlaylists.php";
+
+use SimeonBorko\WpYoutubeAgent\Persistence;
+
+$mysqli = new mysqli("127.0.0.1", "wpuser", "wppassword", "wpdb", 3307);
+
+$playlists = Persistence\getWpPlaylists($mysqli);
+
+?>
+
+<table border="1">
+<?php
+foreach ($playlists as $p):
+?>
+    <tr>
+        <td><?= $p->id ?></td>
+        <td><?= $p->title ?></td>
+        <td><?= $p->description ?></td>
+        <td>
+            <?php if ($p->imageUrl): ?>
+                <img width="150" src="<?= $p->imageUrl ?>">
+            <?php endif; ?>
+        <td>
+    </tr>
+<?php
+endforeach;
+?>
+</table>
+
+
+<?php
+
+$mysqli->close();
