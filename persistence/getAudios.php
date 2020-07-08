5c638dbcc943e261d00bd96ddf189f8543636cbb
Simeon Borko <simeon.borko@gmail.com>
2020-07-08 13:57:29 +0200

    getAudios

--- persistence/getAudios.php
+++ persistence/getAudios.php
@@ -7,5 +7,16 @@ use SimeonBorko\WpYoutubeAgent\Entity;
 // param rssUrl: string
 // return: list of Audio
 function getAudios($rssUrl) {
-  // TODO
+  $rssStr = \file_get_contents($rssUrl);
+  $root = new \SimpleXMLElement($rssStr);
+  $list = array();
+  foreach ($root->channel->item as $item) {
+    $audio = new Entity\Audio();
+    $audio->id = (string) $item->guid;
+    $audio->title = (string) $item->title;
+    $audio->audioUrl = (string) $item->enclosure['url'];
+    $audio->description = (string) $item->description;
+    $list[] = $audio;
+  }
+  return $list;
 }
